874d26b918f7691c9640e9dc14ba4a646fbdac2a
nemectomas84-svg <nemec.tomas84@gmail.com>
2026-05-29 16:04:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot V2 beží 🚀"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print("Webhook prijatý:", data)
+
+    return jsonify({"status": "ok"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
